e1b5ef2a3103b583e923693aa4bb6c9a8bfc19c5
MasihNoob <neraka.malam@gmail.com>
2018-07-11 21:59:05 +0700

    sebelum perubahan baru

--- app/src/main/java/com/example/xiinlaw/ngalamkipa/MainActivity.java
+++ app/src/main/java/com/example/xiinlaw/ngalamkipa/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.xiinlaw.ngalamkipa;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void NextMethod(View view) {
+        switch (view.getId()){
+            case R.id.glintundBtn:{
+                Common.setLatGreen("-7.946027");
+                Common.setLngGreen("112.638604");
+                Common.setPlaceName("Go Green Glintung");
+            }
+            break;
+            case R.id.kampungDBtn:{
+                Common.setLatGreen("-7.981849");
+                Common.setLngGreen("112.6359163");
+                Common.setPlaceName("Kampung Tridi");
+            }
+            break;
+            case R.id.kayutangan:{
+                Common.setLatGreen("-7.9814255");
+                Common.setLngGreen("112.628141");
+                Common.setPlaceName("kajoetangan heritage");
+            }
+            break;case R.id.warna:{
+                Common.setLatGreen("-7.9832215");
+                Common.setLngGreen("112.6354428");
+                Common.setPlaceName("warna warni jodipan");
+            }
+            break;
+        }
+        startActivity(new Intent(this, MapsActivity.class));
     }
 }
